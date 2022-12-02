716250de5d4efbc4a28d770e8b7d34a8cc30e0fe
Gabriel Pereira <gabrielpedepera@gmail.com>
2022-12-02 22:59:31 +0000

    Add challenge for the second day

---
+++ rock_paper_scissors/lib/rock_paper_scissors.ex
@@ -0,0 +1,18 @@
+defmodule RockPaperScissors do
+  @moduledoc """
+  Documentation for `RockPaperScissors`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RockPaperScissors.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
