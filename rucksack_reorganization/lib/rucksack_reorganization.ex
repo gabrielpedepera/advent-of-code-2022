9362e2618bec57c22f27ca14cf4eeb6e3a57aae5
Gabriel Pereira <gabrielpedepera@gmail.com>
2022-12-03 22:12:12 +0000

    Add project for the third day

---
+++ rucksack_reorganization/lib/rucksack_reorganization.ex
@@ -0,0 +1,18 @@
+defmodule RucksackReorganization do
+  @moduledoc """
+  Documentation for `RucksackReorganization`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RucksackReorganization.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ rucksack_reorganization/test/rucksack_reorganization_test.exs
@@ -0,0 +1,8 @@
+defmodule RucksackReorganizationTest do
+  use ExUnit.Case
+  doctest RucksackReorganization
+
+  test "greets the world" do
+    assert RucksackReorganization.hello() == :world
+  end
+end
